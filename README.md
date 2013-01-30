d219ee3651094e2c51d9760e4c469e8b547d3fe8
Kasper Mikiewicz <IderedPL@gmail.com>
2013-01-30 19:17:44 +0100

    first release

---
+++ README.md
@@ -0,0 +1,73 @@
+# Zen Forms
+
+Zen Forms plugin provides you with distraction mode that you can add to your forms or single inputs, textareas.
+
+## Docs
+
+You can add zen mode for all fields in form or single one. At the moment supported are only text inputs and textareas.
+
+### Add zen for all supported elements in form
+
+**HTML**
+```
+<form class="zen-mode">
+	<a href="#" class="go-zen">Open zen mode</a>
+	<input type="text" name="name" placeholder="Type your name here.">
+	<textarea name="bio" placeholder="Write your bio here..."></textarea>
+	<input type="submit" name="submit" value="Send">
+</form>
+```
+
+.go-zen class is the default class that opens zen mode.
+
+**JS**
+```
+$('.zen-mode').zenForms();
+```
+
+### Add zen mode for single element
+
+Bellow you can see how to change theme and use custom element as a trigger that opens zen mode.
+
+**HTML**
+```
+<form>
+	<a href="#" class="open-zen-mode">Open zen mode</a>
+	<input type="text" name="name" placeholder="Type your name here.">
+	<textarea name="bio" placeholder="Write your bio here..." class="zen-mode"></textarea>
+	<input type="submit" name="submit" value="Send">
+</form>
+```
+
+**JS**
+```
+$('.zen-mode').zenForms({
+	trigger: '.open-zen-mode',
+	theme: 'light'
+});
+```
+
+## Settings
+
+* trigger ('.go-zen') - element the opens zen mode on click
+* theme   ('dark')
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+**MIT Licensing**
+
+Copyright (c) 2013 Kasper Mikiewicz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ zen-forms.js
@@ -0,0 +1,169 @@
+/** Zen Forms 1.0.0 | MIT License | git.io/zen-forms */
+
+(function($) {
+
+    $.fn.zenForms = function(settings) {
+
+        settings = $.extend({
+            trigger: '.go-zen',
+            theme: 'dark'
+        }, settings);
+
+        /**
+         * Helper functions
+         */
+        var Utils = {
+
+            watchEmpty: function($form) {
+
+                $form.find('input, textarea').each(function() {
+                   $(this).on('change', function() {
+                        $(this)[ $(this).val() ? 'removeClass' : 'addClass' ]('empty');
+                   }).trigger('change');
+                });
+
+            }
+
+        }, // Utils
+
+        /**
+         * Core functionality
+         */
+        App = {
+
+            /**
+             * Wrapper element
+             */
+            Environment: null,
+
+            /**
+             * Functions to create and manipulate environment
+             */
+            env: {
+
+                create: function() {
+
+                    var theme = settings.theme == 'dark' ? '' : ' light-theme';
+
+                    return $('<div>', {class: 'zen-forms' + theme}).hide().appendTo('body').fadeIn(200);
+
+                }, // create
+
+                /**
+                 * Update orginal inputs with new values and destroy Environment
+                 */
+                destroy: function($elements) {
+
+                    // Update orginal inputs with new values
+                    $elements.each(function(i) {
+                       $(this).val($('#zen-forms-input' + i).val());
+                    });
+
+                    $('.zen-forms').fadeOut(200, function() {
+                        $(this).remove();
+                    });
+
+                }, // destroy
+
+                /**
+                 * Append inputs, textareas to Environment
+                 */
+                add: function($elements) {
+
+                    $elements.each(function(i) {
+
+                        var $this    = $(this),
+                            $wrapper = App.env.addObject(App.Environment, 'div', {class: 'zen-forms-input-wrap'}),
+
+                            value = $this.val(),
+                            id    = $this.attr('id'),
+                            ID    = 'zen-forms-input' + i,
+                            label = $("label[for=" + id + "]").text() || $this.attr('placeholder') || value;
+
+                        // Exclude specified elements
+                        if ( $.inArray( $this.attr('type'), ['checkbox', 'radio', 'submit']) == -1) {
+
+                            if ( $this.is('input') )
+                                App.env.addObject($wrapper, 'input', {
+                                    id: ID,
+                                    value: value,
+                                    type: $this.attr('type')
+                                });
+                            else
+                                App.env.addObject($wrapper, 'textarea', {
+                                    id: ID,
+                                    text: value
+                                });
+
+                            App.env.addObject($wrapper, 'label', {
+                                for: ID,
+                                text: label
+                            });
+
+                        }
+
+                    });
+
+                }, // add
+
+                /**
+                 * Wrapper for creating jQuery objects
+                 */
+                addObject: function($wrapper, type, params, fn, fnMethod) {
+
+                    return $('<'+type+'>', params).on(fnMethod || 'click', fn).appendTo($wrapper);
+
+                }, // addObject
+
+                switchTheme: function() {
+
+                    App.Environment.toggleClass('light-theme');
+
+                } // switchTheme
+
+            }, // env
+
+            zen: function($elements) {
+
+                // Create environment
+                App.Environment = App.env.create();
+
+                // Add close button
+                App.env.addObject(App.Environment, 'a', {
+                    class: 'zen-forms-close-button',
+                    html: '&times;'
+                }, function() {
+                    App.env.destroy($elements);
+                });
+
+                // Add theme switch button
+                App.env.addObject(App.Environment, 'a', {
+                    class: 'zen-forms-theme-switch',
+                    html: '&bull;'
+                }, function() {
+                    App.env.switchTheme();
+                });
+
+                // Add inputs and textareas from form
+                App.env.add($elements);
+
+                // Watch inputs and add "empty" class if needed
+                Utils.watchEmpty(App.Environment);
+
+            }, // zen
+
+        }; // App
+
+        return this.each(function() {
+
+            var $this = $(this);
+
+            $(settings.trigger).on('click', function() {
+                App.zen( $this.is('form') ? $this.find('input, textarea') : $this );
+            });
+
+        });
+
+    };
+
+})(jQuery);
