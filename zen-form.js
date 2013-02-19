37a268435e91889129531e8af783363544fe1270
Kasper Mikiewicz <IderedPL@gmail.com>
2013-02-19 01:47:45 +0100

    add select support, new icons

--- zen-form.js
+++ zen-form.js
@@ -1,4 +1,4 @@
-/** Zen Forms 1.0.0 | MIT License | git.io/zen-form */
+/** Zen Forms 1.0.1 | MIT License | git.io/zen-form */
 
 (function($) {
 
@@ -16,10 +16,43 @@
 
             watchEmpty: function($form) {
 
-                $form.find('input, textarea').each(function() {
+                $form.find('input, textarea, select').each(function() {
+
                    $(this).on('change', function() {
+
                         $(this)[ $(this).val() ? 'removeClass' : 'addClass' ]('empty');
+
                    }).trigger('change');
+
+                });
+
+            },
+
+            customSelect: function($select, $customSelect) {
+
+                var $selected;
+
+                $customSelect.on('click', function(event) {
+
+                    event.stopPropagation();
+
+                    $selected = $customSelect.find('.selected');
+
+                    $customSelect.toggleClass('is-open');
+
+                    if ( $customSelect.hasClass('is-open') ) {
+                        $customSelect.scrollTop(
+                            $selected.position().top - $selected.outerHeight()
+                        );
+                    }
+
+
+                }).find('a').on('click', function() {
+
+                    $(this).addClass('selected').siblings().removeClass('selected');
+
+                    $select.val($(this).data('value'));
+
                 });
 
             }
@@ -41,11 +74,17 @@
              */
             env: {
 
+
+                /**
+                 * Object where elements created with App.env.addObject are appended
+                 */
+                wrapper: null,
+
                 create: function() {
 
-                    var theme = settings.theme == 'dark' ? '' : ' light-theme';
-
-                    return $('<div>', {class: 'zen-forms' + theme}).hide().appendTo('body').fadeIn(200);
+                    return $('<div>', {
+                        class: 'zen-forms' + (settings.theme == 'dark' ? '' : ' light-theme')
+                    }).hide().appendTo('body').fadeIn(200);
 
                 }, // create
 
@@ -64,8 +103,13 @@
 
                     });
 
-                    $('.zen-forms').fadeOut(200, function() {
-                        $(this).remove();
+                    // Hide and remove Environment
+                    App.Environment.fadeOut(200, function() {
+
+                        App.env.wrapper = null;
+
+                        App.Environment.remove();
+
                     });
 
                 }, // destroy
@@ -75,35 +119,39 @@
                  */
                 add: function($elements) {
 
+                    var $el, $label, value, id, IE, label;
+
                     $elements.each(function(i) {
 
-                        var $this    = $(this),
-                            $wrapper = App.env.addObject(App.Environment, 'div', {class: 'zen-forms-input-wrap'}),
+                        App.env.addWrapper('div', { class: 'zen-forms-input-wrap' });
 
-                            value = $this.val(),
-                            id    = $this.attr('id'),
-                            ID    = 'zen-forms-input' + i,
-                            label = $("label[for=" + id + "]").text() || $this.attr('placeholder') || '';
+                        $el = $(this);
+
+                        value = $el.val();
+
+                        id = $el.attr('id');
+
+                        ID = 'zen-forms-input' + i;
+
+                        label = $el.data('label') || $("label[for=" + id + "]").text() || $el.attr('placeholder') || '';
 
                         // Exclude specified elements
-                        if ( $.inArray( $this.attr('type'), ['checkbox', 'radio', 'submit']) == -1) {
+                        if ( $.inArray( $el.attr('type'), ['checkbox', 'radio', 'submit']) == -1) {
 
-                            if ( $this.is('input') )
-                                App.env.addObject($wrapper, 'input', {
-                                    id: ID,
-                                    value: value,
-                                    type: $this.attr('type')
-                                });
+                            if ( $el.is('input') )
+                                App.env.addInput($el, ID, value);
+                            else if ( $el.is('select') )
+                                App.env.addSelect($el, ID);
                             else
-                                App.env.addObject($wrapper, 'textarea', {
-                                    id: ID,
-                                    text: value
-                                });
+                                App.env.addTextarea($el, ID, value);
 
-                            App.env.addObject($wrapper, 'label', {
+                            $label = App.env.addObject('label', {
                                 for: ID,
                                 text: label
                             });
+
+                            if ( $el.is('select') )
+                                $label.prependTo(App.env.wrapper);
 
                         }
 
@@ -111,12 +159,85 @@
 
                 }, // add
 
+                addInput: function($input, ID, value) {
+
+                    return App.env.addObject('input', {
+                        id: ID,
+                        value: value,
+                        class: 'input',
+                        type: $input.attr('type')
+                    });
+
+                },
+
+                addTextarea: function($textarea, ID, value) {
+
+                    return App.env.addObject('textarea', {
+                        id: ID,
+                        text: value,
+                        rows: 5,
+                        class: 'input'
+                    });
+
+                },
+
+                addSelect: function($orginalSelect, ID) {
+
+                    var $select = App.env.addObject('select', {
+                            id: ID,
+                            class: 'select'
+                        }),
+                        $options = $orginalSelect.find('option'),
+                        $customSelect = App.env.addObject('div', {
+                            class: 'custom-select-wrap',
+                            html: '<div class="custom-select"></div>'
+                        }).children();
+
+                    $select.append($options.clone());
+
+                    $.each($options, function(i, option) {
+
+                        App.env.createObject('a', {
+                            href: '#',
+                            html: '<span>' + $(option).text() + '</span>' ,
+                            'data-value': $(option).attr('value'),
+                            class: $(option).prop('selected') ? 'selected' : ''
+                        }).appendTo($customSelect);
+
+                    });
+
+                    $select.val($orginalSelect.val());
+
+                    Utils.customSelect($select, $customSelect);
+
+                    return $customSelect;
+
+                },
+
                 /**
                  * Wrapper for creating jQuery objects
                  */
-                addObject: function($wrapper, type, params, fn, fnMethod) {
+                createObject: function(type, params, fn, fnMethod) {
 
-                    return $('<'+type+'>', params).on(fnMethod || 'click', fn).appendTo($wrapper);
+                    return $('<'+type+'>', params).on(fnMethod || 'click', fn);
+
+                }, // addObject
+
+                /**
+                 * Wrapper for adding jQuery objects to wrapper
+                 */
+                addObject: function(type, params, fn, fnMethod) {
+
+                    return App.env.createObject(type, params, fn, fnMethod).appendTo(App.env.wrapper || App.Environment);
+
+                }, // addObject
+
+                /**
+                 * Wrapper for creating jQuery "wrapper" element
+                 */
+                addWrapper: function(type, params) {
+
+                    return App.env.wrapper = App.env.createObject(type, params).appendTo(App.Environment);
 
                 }, // addObject
 
@@ -134,17 +255,17 @@
                 App.Environment = App.env.create();
 
                 // Add close button
-                App.env.addObject(App.Environment, 'a', {
+                App.env.addObject('a', {
                     class: 'zen-forms-close-button',
-                    html: '&times;'
+                    html: '<i class="zen-icon zen-icon--close"></i>'
                 }, function() {
                     App.env.destroy($elements);
                 });
 
                 // Add theme switch button
-                App.env.addObject(App.Environment, 'a', {
+                App.env.addObject('a', {
                     class: 'zen-forms-theme-switch',
-                    html: '&bull;'
+                    html: '<i class="zen-icon zen-icon--theme"></i>'
                 }, function() {
                     App.env.switchTheme();
                 });
@@ -154,6 +275,11 @@
                     if (e.which == 27) {
                         App.env.destroy($elements);
                     }
+                });
+
+                // Hide any elements(mostly selects) when clicked outside them
+                $(document).on('click', function() {
+                    $('.is-open').removeClass('is-open');
                 });
 
                 // Add inputs and textareas from form
@@ -172,7 +298,7 @@
 
             $(settings.trigger).on('click', function(event) {
                 event.preventDefault();
-                App.zen( $this.is('form') ? $this.find('input, textarea') : $this );
+                App.zen( $this.is('form') ? $this.find('input, textarea, select') : $this );
             });
 
         });
